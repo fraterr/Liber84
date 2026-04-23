edd842bc513c0d6891f85a7309a37cac4cad6986
fraterr <126991606+fraterr@users.noreply.github.com>
2026-04-23 22:35:01 +0200

    Update vite.config.js

--- enochian-explorer/vite.config.js
+++ enochian-explorer/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Liber84/',  // <-- AGGIUNGI QUESTA RIGA (attento alla virgola finale!)
   plugins: [react()],
 })
